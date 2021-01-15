3178b21cd748e5592b8e488923c9741f160746a2
MiyueSC <913784771@qq.com>
2021-01-15 22:29:44 +0800

    Create ElementFormConfig.vue

---
+++ package/process-panel/form-config/ElementFormConfig.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="panel-tab__content">
+    表单配置部分
+  </div>
+</template>
+<script>
+export default {
+  name: "ElementFormConfig",
+  props: {
+    listeners: {
+      type: Array,
+      default: () => []
+    },
+    bpmnModeler: Object,
+    elementId: String
+  },
+  inject: {
+    prefix: "prefix",
+    width: "width"
+  },
+  data() {
+    return {};
+  },
+  created() {},
+  computed: {},
+  watch: {},
+  methods: {}
+};
+</script>
