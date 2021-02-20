ddee34b0577b85d6b767a7e0423faf9484f38353
MiyueSC <913784771@qq.com>
2021-02-20 09:54:27 +0800

    feat：优化打包

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,38 @@
-const CompressionWebpackPlugin = require("compression-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 const IS_PROD = process.env.NODE_ENV === "production";
 
+const cdn = {
+  externals: {
+    vue: "Vue",
+    "element-ui": "ELEMENT"
+  },
+  css: [],
+  js: ["https://cdn.bootcdn.net/ajax/libs/vue/2.6.12/vue.min.js", "https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.0/index.min.js"]
+};
+
 module.exports = {
-  publicPath: IS_PROD ? "././" : "/",
+  publicPath: IS_PROD ? "././" : "/", // 打包相对路径
   productionSourceMap: false,
   devServer: {
     port: 8100
   },
+  chainWebpack: config => {
+    // ============注入cdn start============
+    config.plugin("html").tap(args => {
+      // 生产环境或本地需要cdn时，才注入cdn
+      if (IS_PROD) args[0].cdn = cdn;
+      return args;
+    });
+    // ============注入cdn start============
+  },
   configureWebpack: config => {
     // 生产环境相关配置
     if (IS_PROD) {
-      config.externals = {
-        vue: "Vue",
-        "element-ui": "ELEMENT"
-      };
+      // cdn
+      config.externals = cdn.externals;
 
+      // 代码混淆
       config.plugins.push(new TerserPlugin());
     }
   }
