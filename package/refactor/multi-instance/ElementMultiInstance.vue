278a78fb7767b060a6c18b5aeb4866b5725c1658
MiyueSC <913784771@qq.com>
2021-04-02 14:00:26 +0800

    feat: 任务多实例配置

--- package/refactor/multi-instance/ElementMultiInstance.vue
+++ package/refactor/multi-instance/ElementMultiInstance.vue
@@ -12,24 +12,29 @@
     </el-form-item>
     <template v-if="loopCharacteristics === 'ParallelMultiInstance' || loopCharacteristics === 'SequentialMultiInstance'">
       <el-form-item label="循环基数" key="loopCardinality">
-        <el-input v-model="loopInstanceForm.loopCardinality" clearable />
+        <el-input v-model="loopInstanceForm.loopCardinality.body" clearable @change="updateLoopCardinality" />
       </el-form-item>
       <el-form-item label="集合" key="collection">
-        <el-input v-model="loopInstanceForm.collection" clearable />
+        <el-input v-model="loopInstanceForm.collection" clearable @change="updateLoopBase" />
       </el-form-item>
       <el-form-item label="元素变量" key="elementVariable">
-        <el-input v-model="loopInstanceForm.elementVariable" clearable />
+        <el-input v-model="loopInstanceForm.elementVariable" clearable @change="updateLoopBase" />
       </el-form-item>
       <el-form-item label="完成条件" key="completionCondition">
-        <el-input v-model="loopInstanceForm.completionCondition" clearable />
+        <el-input v-model="loopInstanceForm.completionCondition.body" clearable @change="updateLoopCondition" />
       </el-form-item>
       <el-form-item label="异步状态" key="async">
-        <el-checkbox v-model="loopInstanceForm.asyncBefore" label="异步前" />
-        <el-checkbox v-model="loopInstanceForm.asyncAfter" label="异步后" />
-        <el-checkbox v-model="loopInstanceForm.exclusive" v-if="loopInstanceForm.asyncAfter || loopInstanceForm.asyncBefore" label="排除" />
+        <el-checkbox v-model="loopInstanceForm.asyncBefore" label="异步前" @change="updateLoopAsync('asyncBefore')" />
+        <el-checkbox v-model="loopInstanceForm.asyncAfter" label="异步后" @change="updateLoopAsync('asyncAfter')" />
+        <el-checkbox
+          v-model="loopInstanceForm.exclusive"
+          v-if="loopInstanceForm.asyncAfter || loopInstanceForm.asyncBefore"
+          label="排除"
+          @change="updateLoopAsync('exclusive')"
+        />
       </el-form-item>
-      <el-form-item label="重试周期" prop="failedJobRetryTimeCycle" v-if="loopInstanceForm.asyncAfter || loopInstanceForm.asyncBefore" key="cycle">
-        <el-input v-model="loopInstanceForm.failedJobRetryTimeCycle" clearable />
+      <el-form-item label="重试周期" prop="timeCycle" v-if="loopInstanceForm.asyncAfter || loopInstanceForm.asyncBefore" key="timeCycle">
+        <el-input v-model="loopInstanceForm.timeCycle" clearable @change="updateLoopTimeCycle" />
       </el-form-item>
     </template>
   </el-form>
@@ -42,9 +47,18 @@ export default {
     businessObject: Object,
     type: String
   },
+  inject: {
+    prefix: "prefix"
+  },
   data() {
     return {
       loopCharacteristics: "",
+      //默认配置，用来覆盖原始不存在的选项，避免报错
+      defaultLoopInstanceForm: {
+        completionCondition: { body: "" },
+        loopCardinality: { body: "" },
+        extensionElements: []
+      },
       loopInstanceForm: {}
     };
   },
@@ -52,7 +66,7 @@ export default {
     businessObject: {
       immediate: true,
       handler(val) {
-        console.log("watch", val);
+        console.log("multi instance watch", val);
         this.bpmnElement = window.bpmnInstances.bpmnElement;
         this.getElementLoop(val);
       }
@@ -75,9 +89,24 @@ export default {
       } else {
         this.loopCharacteristics = "ParallelMultiInstance";
       }
+      // 合并配置
+      this.loopInstanceForm = {
+        ...this.defaultLoopInstanceForm,
+        ...businessObject.loopCharacteristics
+      };
+      console.log("form", this.loopInstanceForm, businessObject.loopCharacteristics);
+      // 保留当前元素 businessObject 上的 loopCharacteristics 实例
+      this.multiLoopInstance = window.bpmnInstances.bpmnElement.businessObject.loopCharacteristics;
+      if (
+        businessObject.loopCharacteristics.extensionElements &&
+        businessObject.loopCharacteristics.extensionElements.values &&
+        businessObject.loopCharacteristics.extensionElements.values.length
+      ) {
+        this.$set(this.loopInstanceForm, "timeCycle", businessObject.loopCharacteristics.extensionElements.values[0].body);
+      }
     },
     updateLoopCharacteristics(type) {
-      this.loopInstanceForm = {}; // 切换类型取消原表单配置
+      // this.loopInstanceForm = { ...this.defaultLoopInstanceForm }; // 切换类型取消原表单配置
       // 取消多实例配置
       if (type === "Null") {
         window.bpmnInstances.modeling.updateProperties(this.bpmnElement, { loopCharacteristics: null });
@@ -101,7 +130,57 @@ export default {
       window.bpmnInstances.modeling.updateProperties(this.bpmnElement, {
         loopCharacteristics: this.multiLoopInstance
       });
+    },
+    // 循环基数
+    updateLoopCardinality(cardinality) {
+      let loopCardinality = null;
+      if (cardinality && cardinality.length) {
+        loopCardinality = window.bpmnInstances.moddle.create("bpmn:FormalExpression", { body: cardinality });
+      }
+      window.bpmnInstances.modeling.updateModdleProperties(this.bpmnElement, this.multiLoopInstance, { loopCardinality });
+    },
+    // 完成条件
+    updateLoopCondition(condition) {
+      let completionCondition = null;
+      if (condition && condition.length) {
+        completionCondition = window.bpmnInstances.moddle.create("bpmn:FormalExpression", { body: condition });
+      }
+      window.bpmnInstances.modeling.updateModdleProperties(this.bpmnElement, this.multiLoopInstance, { completionCondition });
+    },
+    // 重试周期
+    updateLoopTimeCycle(timeCycle) {
+      const extensionElements = window.bpmnInstances.moddle.create("bpmn:ExtensionElements", {
+        values: [
+          window.bpmnInstances.moddle.create(`${this.prefix}:FailedJobRetryTimeCycle`, {
+            body: timeCycle
+          })
+        ]
+      });
+      window.bpmnInstances.modeling.updateModdleProperties(this.bpmnElement, this.multiLoopInstance, { extensionElements });
+    },
+    // 直接更新的基础信息
+    updateLoopBase() {
+      window.bpmnInstances.modeling.updateModdleProperties(this.bpmnElement, this.multiLoopInstance, {
+        collection: this.loopInstanceForm.collection || null,
+        elementVariable: this.loopInstanceForm.elementVariable || null
+      });
+    },
+    // 各异步状态
+    updateLoopAsync(key) {
+      const { asyncBefore, asyncAfter } = this.loopInstanceForm;
+      let asyncAttr = Object.create(null);
+      if (!asyncBefore && !asyncAfter) {
+        this.$set(this.loopInstanceForm, "exclusive", false);
+        asyncAttr = { asyncBefore: false, asyncAfter: false, exclusive: false, extensionElements: null };
+      } else {
+        asyncAttr[key] = this.loopInstanceForm[key];
+      }
+      window.bpmnInstances.modeling.updateModdleProperties(this.bpmnElement, this.multiLoopInstance, asyncAttr);
     }
+  },
+  beforeDestroy() {
+    this.multiLoopInstance = null;
+    this.bpmnElement = null;
   }
 };
 </script>
