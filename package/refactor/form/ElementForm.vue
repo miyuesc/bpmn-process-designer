4518fe77ebfda32c6ae4f26f5f725b1a52bc8ab0
MiyueSC <913784771@qq.com>
2021-04-06 12:53:46 +0800

    Create ElementForm.vue

---
+++ package/refactor/form/ElementForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="panel-tab__content">
+    表单配置
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ElementForm",
+  props: {
+    id: String,
+    type: String
+  },
+  inject: {
+    prefix: "prefix",
+    width: "width"
+  },
+  data() {
+    return {
+      fieldList: []
+    };
+  }
+};
+</script>
