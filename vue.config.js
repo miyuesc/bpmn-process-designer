50a678b32df903c2224a4d7011bb526664dd9c30
MiyueSC <913784771@qq.com>
2022-01-06 10:50:45 +0800

    feat: gzip

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 const IS_PROD = process.env.NODE_ENV === "production";
 
@@ -86,6 +87,16 @@ module.exports = {
 
       // 代码混淆
       config.plugins.push(new TerserPlugin());
+      config.plugins.push(
+        new CompressionPlugin({
+          algorithm: "gzip", // 使用gzip压缩
+          test: /\.js$|\.html$|\.css$/, // 匹配文件名
+          // filename: "[path].gz", // 压缩后的文件名(保持原文件名，后缀加.gz)
+          minRatio: 1, // 压缩率小于1才会压缩
+          threshold: 10240, // 对超过10k的数据压缩
+          deleteOriginalAssets: false // 是否删除未压缩的源文件，谨慎设置，如果希望提供非gzip的资源，可不设置或者设置为false（比如删除打包后的gz后还可以加载到原始资源文件）
+        })
+      );
     }
   }
 };
