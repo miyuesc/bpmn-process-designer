1ea8e17935a0e18321addbf97a38d04e72259e51
MiyueSC <913784771@qq.com>
2022-09-27 11:28:24 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,7 @@ function resolve(dir) {
 }
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "././" : "/",
   pages: {
     index: {
       entry: "playground/main.js",
