780e72e8571044d42ab6d1d5a75f8eb2fb2a252a
MiyueSC <913784771@qq.com>
2022-09-30 11:25:36 +0800

    fix: 文件导入异常

--- packages/components/Toolbar/tools/Import.vue
+++ packages/components/Toolbar/tools/Import.vue
@@ -6,26 +6,28 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { catchError } from "@utils/printCatch";
+import { getModeler } from "@packages/bpmn-utils/BpmnDesignerUtils";
 
 export default {
   name: "BpmnImport",
-  computed: {
-    ...mapGetters(["getModeler"])
-  },
   methods: {
     openImportWindow() {
       this.$refs.importRef && this.$refs.importRef.click();
     },
     changeImportFile() {
-      if (this.$refs.importRef && this.$refs.importRef.files) {
-        const file = this.$refs.importRef.files[0];
-        const reader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = function () {
-          const xmlStr = this.result;
-          this.getModeler && this.getModeler.importXML(xmlStr);
-        };
+      try {
+        if (this.$refs.importRef && this.$refs.importRef.files) {
+          const file = this.$refs.importRef.files[0];
+          const reader = new FileReader();
+          reader.readAsText(file);
+          reader.onload = function () {
+            const xmlStr = this.result;
+            getModeler() && getModeler().importXML(xmlStr);
+          };
+        }
+      } catch (e) {
+        catchError(e);
       }
     }
   }
